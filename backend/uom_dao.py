65a1f3801a252010cc557f386939a836aca7779e
muhammad489 <137202673+muhammad489@users.noreply.github.com>
2024-09-08 23:38:14 +0530

    Add files via upload

---
+++ backend/uom_dao.py
@@ -0,0 +1,14 @@
+def get_uom(connection):
+    cursor = connection.cursor()
+    query = ("select * from uom")
+    cursor.execute(query)
+    response = []
+    for (uom_id, uom_name) in cursor:
+        response.append({
+            'uom_id': uom_id,
+            'uom_name': uom_name
+        })
+    return response
+
+if __name__ == '__main__':
+    from sql_connection import get_sql_connection
